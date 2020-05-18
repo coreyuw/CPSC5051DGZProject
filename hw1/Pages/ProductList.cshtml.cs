b787223204972de16c903619a25a131e09faa395
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2020-05-17 21:32:47 -0700

    move homework work to ProductList page

--- hw1/Pages/ProductList.cshtml.cs
+++ hw1/Pages/ProductList.cshtml.cs
@@ -2,16 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
+using hw1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace hw1.Pages
 {
     public class ProductListModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public ProductListModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
