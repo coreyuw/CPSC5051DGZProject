14b4d349bdd0a757a81ce3090c21c078e71cbe17
coreyuw <zzhou@seattleu.edu>
2020-05-17 17:19:00 -0700

    Initial check-in of module hw1

---
+++ hw1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace hw1.Models
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        public string Maker { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+
+    }
+}
