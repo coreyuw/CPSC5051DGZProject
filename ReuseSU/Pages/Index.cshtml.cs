97b68d0853ccd41d675647e3e3630dd3285b22d1
Zhiyang Zhou <39231660+coreyuw@users.noreply.github.com>
2020-05-29 16:34:29 -0700

    Fixed home page layout on index.cshtml using the product example used in tutorial

--- ReuseSU/Pages/Index.cshtml.cs
+++ ReuseSU/Pages/Index.cshtml.cs
@@ -12,10 +12,19 @@ namespace ReuseSU.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
 
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
