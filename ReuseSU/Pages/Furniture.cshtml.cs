2deac35c169645a8fe1421dd12bf0e15d372a18c
Zhiyang Zhou <39231660+coreyuw@users.noreply.github.com>
2020-05-29 23:38:26 -0700

    Added files Furniture.cshtml and Furniture.cshtml.cs for category filtering demo
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/s6P36xUrXkO_13vkWjrxg2QAMbiK?Type=TaskLink&Channel=Link&CreatedTime=637264174929370000

---
+++ ReuseSU/Pages/Furniture.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
+using ReuseSU.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ReuseSU.Pages
+{
+    public class FurnitureModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public FurnitureModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
