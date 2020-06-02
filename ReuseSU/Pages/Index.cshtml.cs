faf3818a6ef81909e8f0582ecb6c3ec237adb789
Zhiyang Zhou <39231660+coreyuw@users.noreply.github.com>
2020-06-01 22:04:15 -0700

    Added variable comments and function summaries for index.cshtml.cs home razor page
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/slOhsN6jbES_1FpAUjgN92QABm3m?Type=TaskLink&Channel=Link&CreatedTime=637266710481810000

--- ReuseSU/Pages/Index.cshtml.cs
+++ ReuseSU/Pages/Index.cshtml.cs
@@ -10,18 +10,37 @@ using Microsoft.Extensions.Logging;
 
 namespace ReuseSU.Pages
 {
+    /// <summary>
+    /// Model class for index.cshtml.cs home page
+    /// </summary>
     public class IndexModel : PageModel
     {
+        // used for logging data
         private readonly ILogger<IndexModel> _logger;
+
+        // product service declaration
         public JsonFileProductService ProductService;
+
+        /// <summary>
+        /// products Enumerable instantiation
+        /// </summary>
         public IEnumerable<Product> Products { get; private set; }
 
+        /// <summary>
+        /// the model of the index.cshtml.cs page
+        /// </summary>
+        /// <param name="logger">logging data</param>
+        /// <param name="productService">product service</param>
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
             _logger = logger;
             ProductService = productService;
         }
+
+        /// <summary>
+        /// function called when the page is loaded
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetProducts();
