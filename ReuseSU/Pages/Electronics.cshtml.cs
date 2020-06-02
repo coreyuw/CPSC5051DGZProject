59ae2945c59f057595e95482218981694881d76a
Zhiyang Zhou <39231660+coreyuw@users.noreply.github.com>
2020-06-01 22:22:14 -0700

    added function summaries for the razor page of Electronics.cshtml.cs
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/sxKcRWucs0OaP5WHlwq-eGQAGeWm?Type=TaskLink&Channel=Link&CreatedTime=637266718646420000

--- ReuseSU/Pages/Electronics.cshtml.cs
+++ ReuseSU/Pages/Electronics.cshtml.cs
@@ -7,11 +7,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ReuseSU.Pages
 {
+    /// <summary>
+    /// Model class for the ELectronics.cshtml.cs home page
+    /// </summary>
     public class ElectronicsModel : PageModel
     {
+
+        /// <summary>
+        /// Function called when the page is loaded
+        /// </summary>
         public void OnGet()
         {
 
         }
+
     }
+
 }
