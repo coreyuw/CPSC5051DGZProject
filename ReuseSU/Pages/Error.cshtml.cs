e626f179ecd9a1c01394fc07dfe0fa9fe23889ea
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2020-06-01 23:44:57 -0700

    add comments to error page

--- ReuseSU/Pages/Error.cshtml.cs
+++ ReuseSU/Pages/Error.cshtml.cs
@@ -10,19 +10,26 @@ using Microsoft.Extensions.Logging;
 namespace ReuseSU.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+    /// Model class for the ClaimItem.cshtml.cs page 
     public class ErrorModel : PageModel
     {
+        // the unique id for each request
         public string RequestId { get; set; }
 
+        // shows request id
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        // stores log of errors
         private readonly ILogger<ErrorModel> _logger;
 
+        //  initialize logger
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        // on get request that fails log id
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
