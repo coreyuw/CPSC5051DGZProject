e90b2f2d2b9758d5eaeaa5fed8bcdff8b91a432d
Zhiyang Zhou <39231660+coreyuw@users.noreply.github.com>
2020-06-01 22:25:10 -0700

    added function summaries and variable comments for the razor page of Furniture.cshtml.cs
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/sxKcRWucs0OaP5WHlwq-eGQAGeWm?Type=TaskLink&Channel=Link&CreatedTime=637266718646420000

--- ReuseSU/Pages/Furniture.cshtml.cs
+++ ReuseSU/Pages/Furniture.cshtml.cs
@@ -10,21 +10,41 @@ using Microsoft.Extensions.Logging;
 
 namespace ReuseSU.Pages
 {
+    /// <summary>
+    /// Model class for Furniture.cshtml home page
+    /// </summary>
     public class FurnitureModel : PageModel
     {
+        // Logger instantiation
         private readonly ILogger<IndexModel> _logger;
+        // product service instantiation
         public JsonFileProductService ProductService;
+
+        /// <summary>
+        /// products Enumerable initiation function
+        /// </summary>
         public IEnumerable<Product> Products { get; private set; }
 
+        /// <summary>
+        /// Model class constructor
+        /// </summary>
+        /// <param name="logger">logging data</param>
+        /// <param name="productService">product service</param>
         public FurnitureModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
             _logger = logger;
             ProductService = productService;
         }
+
+        /// <summary>
+        /// fucntion called when the page is loaded
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetProducts();
         }
+
     }
+
 }
