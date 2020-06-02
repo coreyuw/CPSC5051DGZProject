64176aaca84d9512dc3c2b112fcf37b0ace2b127
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2020-06-01 23:27:17 -0700

    Add comments to Login Item page

--- ReuseSU/Pages/Login.cshtml.cs
+++ ReuseSU/Pages/Login.cshtml.cs
@@ -7,8 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ReuseSU.Pages
 {
+    /// <summary>
+    /// Model class for the Login.cshtml.cs page
+    /// </summary>
     public class LoginModel : PageModel
     {
+        /// <summary>
+        /// function called
+        /// </summary>
         public void OnGet()
         {
         }
