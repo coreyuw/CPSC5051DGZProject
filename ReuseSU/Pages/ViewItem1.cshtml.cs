ee5d387353700bf1f1f98d34e8e74a6d685acb61
Zhiyang Zhou <39231660+coreyuw@users.noreply.github.com>
2020-06-01 22:55:30 -0700

    Added function summaries in ViewItem1.cshtml.cs razor page
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/S7DfFue2TUaeFzkIes4UOmQABtdS?Type=TaskLink&Channel=Link&CreatedTime=637266724697510000

--- ReuseSU/Pages/ViewItem1.cshtml.cs
+++ ReuseSU/Pages/ViewItem1.cshtml.cs
@@ -7,11 +7,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ReuseSU.Pages
 {
+    /// <summary>
+    /// Model class for ViewItem1.cshtml page
+    /// </summary>
     public class ViewItem1Model : PageModel
     {
+
+        /// <summary>
+        /// function called when the page is loaded
+        /// </summary>
         public void OnGet()
         {
 
         }
+
     }
+
 }
